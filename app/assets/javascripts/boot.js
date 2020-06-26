82b6874eae6477faf31eae323fc94f07d17b948a
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-06-26 11:44:02 +0000

    a commit

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -1,3 +1,8 @@
 define("boot", ["jQuery"], function ($) {
   var controller = $("body").data("controller"),
-      action = $("body").data("a
+      action = $("body").data("action");
+
+  try {
+    require("pages." + controller + "." + action);
+  } catch (error) {
+  
