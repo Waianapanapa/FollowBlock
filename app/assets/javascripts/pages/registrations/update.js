e4d19e2108b00950bf5cee0c7b709e2981028af5
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-08-23 08:46:22 +0000

    :pencil:

---
+++ app/assets/javascripts/pages/registrations/update.js
@@ -0,0 +1,8 @@
+
+define("pages.registrations.update",
+[
+  "jQuery",
+  "pages.registrations.form",
+], function($, form) {
+  form.init($("form.edit_user"));
+});
---
+++ app/assets/javascripts/services/http-service.js
@@ -0,0 +1,5 @@
+define("services.http",
+[
+  "jQuery"
+], function ($) {
+  
