ac15b43420bd9cd2e1f4eeb9c4473af662dda04c
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-08-25 10:50:59 +0000

    Updated SQL

--- app/assets/javascripts/services/http-service.js
+++ app/assets/javascripts/services/http-service.js
@@ -7,4 +7,7 @@ define("services.http",
   HttpService.prototype.get = function (url, successHandler, errorHandler) {
     $.ajax({
       url: url,
-   
+      method: "GET",
+      dataType: "json"
+    }).done(function (data) {
+      successHand
