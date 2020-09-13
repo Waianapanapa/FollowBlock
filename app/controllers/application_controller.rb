dd8506e46930432ca2d72ca2b842fda13b962198
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-09-13 20:04:06 +0000

    ALSA: hda - Add fixup for ASUS N56VZ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :a
+  before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(_)
+    dashboard_path
+  end
