730da63e7fb66ae3b595dcbe4e0241a06a07a745
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-09-18 05:36:03 +0000

    Miscellaneous fixes.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -5,4 +5,8 @@ class BidsController < ApplicationController
     @bids = Bid.most_recent
                .where.not(user: current_user)
                .where.not(id: Order.where(user: current_user).pluck(:id))
-               .reject { |bid| bid.created_at + bid.numbe
+               .reject { |bid| bid.created_at + bid.number_of_days.days > Time.zone.now }
+  end
+
+  def index
+    @bids = current_user.bids.mo
