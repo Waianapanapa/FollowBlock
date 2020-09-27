1deda0d8cf82ae61de226292a491246b6f5bd875
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-09-27 18:07:47 +0000

    automated commit from rosetta for sim/lib joist, locale el

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,5 @@ class MessagesController < ApplicationController
   respond_to :json
 
   def create
-    @bid = Bid.find(params[:bid_
+    @bid = Bid.find(params[:bid_id])
+    @message = @bid.messages.create(message_params.merge(user: cur
