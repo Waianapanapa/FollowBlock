7e096a4dba0d62d0a26215ae1ab5ec94aae5449d
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-09-29 11:28:55 +0000

    Updating examples

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,11 @@ class MessagesController < ApplicationController
 
   def create
     @bid = Bid.find(params[:bid_id])
-    @message = @bid.messages.create(message_params.merge(user: cur
+    @message = @bid.messages.create(message_params.merge(user: current_user))
+
+    render json: @message
+  end
+
+  private
+
+  def message_param
