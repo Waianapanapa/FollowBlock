57976ddf52b2ca34230cc53b79712303e47f26d9
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-10-11 20:37:55 +0000

    sata_sil: disable trim

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,3 +14,11 @@ class OrdersController < ApplicationController
   def create
     @bid = Bid.find(params[:bid_id])
     @order = current_user.orders.build(bid: @bid)
+
+    if @order.save && @bid.progress!
+      redirect_to bids_path, notice: "Oferta realizada com sucesso."
+    else
+      render :new
+    end
+  end
+end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,1 @@
+class RegistrationsController < Devise::RegistrationsControll
