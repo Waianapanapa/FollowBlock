a092a440454f864173dafa62a5953b37c649cd61
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-10-18 21:00:49 +0000

    alter post

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,5 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |user|
-      user.permit(:email, :password, :p
+      user.permit(:email, :password, :password_confirmation, :personable_type,
+                  :first_name, :last_name, :cpf, :corpor
