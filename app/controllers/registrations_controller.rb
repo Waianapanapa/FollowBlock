dc1ec2387a302ecc19e85f504bc2605dbfc3b8c8
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-10-18 13:54:42 +0000

    Update pageview.json

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,3 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_s
