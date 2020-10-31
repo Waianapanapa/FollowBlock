7eba66027c07154251e7ff9f618699dd63f073bf
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-10-31 22:18:05 +0000

    Update composer files.

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,2 +1,7 @@
 module BootstrapFlashHelper
   ALERT_TYPES = [:success, :info, :warning, :danger] unless const_defined?(:ALERT_TYPES)
+
+  def bootstrap_flash(options = {})
+    flash_messages = []
+
+    flash.each do
