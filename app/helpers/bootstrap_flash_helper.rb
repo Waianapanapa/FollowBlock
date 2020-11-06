fc56c04b556b65a1c04451854648f57e9e807c1f
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2020-11-06 19:22:16 +0000

    fix shebang

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -8,4 +8,6 @@ module BootstrapFlashHelper
       # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
       next if message.blank?
 
-      
+      type = type.to_sym
+      type = :success if type == :notice
+      type = :danger
