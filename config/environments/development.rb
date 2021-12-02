433b6c79f5f389f0bd8f01e4838d3960281b71c9
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2021-12-02 12:50:06 +0000

    Added new theme

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,6 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Raises error for missing translatio
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+end
