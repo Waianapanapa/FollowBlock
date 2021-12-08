20782ea8c9d8766caa5cd2a7d760887067c58b53
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2021-12-08 05:25:20 +0000

    Change default values

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,5 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, al
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better
