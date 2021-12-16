222c4c58fa4162539816cd5fd77d6ae134d5f35e
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2021-12-16 14:44:52 +0000

    changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,4 +18,6 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = t
+  # config.action_dispatch.rack_cache = true
+
+  # Disable serving static files from the `/public` folder by default
