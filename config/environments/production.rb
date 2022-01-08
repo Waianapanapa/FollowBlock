d098972ddd7c9e5ca8c6efcc1037b2c18a391c9c
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-01-08 17:26:03 +0000

    ScadaDoc: update history

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,3 +53,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+
+  # Use a different cache store in production.
+  # config.cache
