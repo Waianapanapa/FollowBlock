db5a321dbd784feb9b5531249f30e284781640e6
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-11 21:41:15 +0000

    add save

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
-  config.consider_al
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = fal
