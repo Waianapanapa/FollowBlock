c35b017d00c76232c5f0a9a7f13e46caa7a2c535
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-11 12:31:01 +0000

    Fixtures

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,4 +14,7 @@ Rails.application.configure do
 
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
-  config.static_cache_control = 'public, max-ag
+  config.static_cache_control = 'public, max-age=3600'
+
+  # Show full error reports and disable caching.
+  config.consider_al
