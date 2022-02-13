dce2fcebe5f921073041f5a02136d5e57f78ef58
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-13 05:30:32 +0000

    Menu fixes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,4 +18,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = fal
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispa
