a03870ddc3a4e33e289bbf823572dcea80e11e7b
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-25 07:16:54 +0000

    Adding travis CI

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,4 @@
 if Rails.env.development?
   # Define settings for mailcatcher
   ActionMailer::Base.tap do |action_mailer|
-    
+    action_mailer.default_url_options = { host: "localhost", port: (ENV["PORT"] || 3
