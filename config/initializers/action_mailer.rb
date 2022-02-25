73d475e9c969fe669ecc5d183ec62bcdfc29b680
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-25 09:13:59 +0000

    ld/

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,6 @@
 if Rails.env.development?
   # Define settings for mailcatcher
   ActionMailer::Base.tap do |action_mailer|
-    action_mailer.default_url_options = { host: "localhost", port: (ENV["PORT"] || 3
+    action_mailer.default_url_options = { host: "localhost", port: (ENV["PORT"] || 3000).to_i }
+    action_mailer.delivery_method = :smtp
+    action_mailer.perform_del
