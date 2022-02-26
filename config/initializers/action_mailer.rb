667f90fa0e1a9f5749981bd5b52ede98a2940e7c
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-02-26 17:28:25 +0000

    fixes #1

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -5,4 +5,6 @@ if Rails.env.development?
     action_mailer.delivery_method = :smtp
     action_mailer.perform_deliveries = true
     action_mailer.raise_delivery_errors = false
-    action_m
+    action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  end
+end
