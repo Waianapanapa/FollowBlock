24a06835f5fd72b25e4ec11ba60fe2ecedea22a0
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-12 22:53:28 +0000

    Refactor in progress

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,3 +7,21 @@ before_fork do |server, worker|
   Signal.trap "TERM" do
     puts "Unicorn master intercepting TERM and sending myself QUIT instead"
     Process.kill "QUIT", Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap "TERM" do
+    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
+  end
+
+  if defined? ActiveRecord::Base
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    config["reaping_frequency"] = (ENV["DB_REAPING_FREQUENCY"] || 10).to_i
+    config["pool"] = (ENV["DB_POOL"] || 2).to_i
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
---
+++ db/migrate/20150407032948_devise_create_users.rb
@@ -0,0 +1,1 @@
+class DeviseCreateUsers < ActiveRecord::Migratio
