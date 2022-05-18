eacb38441c1b76d02363c12c334f93368208390f
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-18 20:11:38 +0000

    spelling error

--- db/migrate/20150407032948_devise_create_users.rb
+++ db/migrate/20150407032948_devise_create_users.rb
@@ -6,4 +6,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.st
+      t.string   :reset_password_token
+      t.datetime :reset_password_se
