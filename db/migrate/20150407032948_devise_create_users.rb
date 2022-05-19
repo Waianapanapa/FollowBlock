89d6caeb440b99854c0c871a71dbf3efc7bf5aea
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-19 21:13:51 +0000

    added editor config

--- db/migrate/20150407032948_devise_create_users.rb
+++ db/migrate/20150407032948_devise_create_users.rb
@@ -12,4 +12,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Rememberable
       t.datetime :remember_created_at
 
-      #
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.
