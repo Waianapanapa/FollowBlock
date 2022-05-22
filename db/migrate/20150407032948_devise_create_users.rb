1b5a4ba2fb9c011f983d9b47de6c5a9f175a8375
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-22 16:40:53 +0000

    Add resources

--- db/migrate/20150407032948_devise_create_users.rb
+++ db/migrate/20150407032948_devise_create_users.rb
@@ -20,4 +20,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.inet     :last_sign_in_ip
 
       t.timestamps
- 
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :reset
