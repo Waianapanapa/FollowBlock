ae72f11f240929d09934e83d90f40a62903c5d8a
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-22 22:50:10 +0000

    minor reformat

--- db/migrate/20150407032948_devise_create_users.rb
+++ db/migrate/20150407032948_devise_create_users.rb
@@ -23,4 +23,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email, unique: true
-    add_index :users, :reset
+    add_index :users, :reset_password_token, unique: true
+  end
+end
---
+++ db/migrate/20150414034414_add_profile_to_users.rb
@@ -0,0 +1,1 @@
+class AddProfileToUsers < ActiveReco
