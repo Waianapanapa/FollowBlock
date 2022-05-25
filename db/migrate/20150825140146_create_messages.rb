9c457815a68c7fdc4ff42a77ee50b7c2ca9c7e6a
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-25 21:19:24 +0000

    Version 0.1.1

--- db/migrate/20150825140146_create_messages.rb
+++ db/migrate/20150825140146_create_messages.rb
@@ -1,3 +1,5 @@
 class CreateMessages < ActiveRecord::Migration
   def change
-    create_tab
+    create_table :messages do |t|
+      t.references :user, null: false, index: true, foreign_key: true
+ 
