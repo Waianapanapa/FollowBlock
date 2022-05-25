5162106cc145ca456abe8b83aa82589e73e0135d
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-25 15:47:02 +0000

    Simplify

--- db/migrate/20150825013108_create_orders.rb
+++ db/migrate/20150825013108_create_orders.rb
@@ -2,4 +2,5 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.references :user, null: false, index: true, foreign_key: true
-     
+      t.references :bid, null: false, index: true, foreign_key: true
+      t.string :st
