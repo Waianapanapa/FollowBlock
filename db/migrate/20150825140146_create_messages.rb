6e3333a863581065c86eeddbd2c21e7cf48966fb
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-05-30 16:46:42 +0000

    update vars

--- db/migrate/20150825140146_create_messages.rb
+++ db/migrate/20150825140146_create_messages.rb
@@ -3,4 +3,9 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.references :user, null: false, index: true, foreign_key: true
       t.references :bid, null: false, index: true, foreign_key: true
-      t.string
+      t.string :text, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
