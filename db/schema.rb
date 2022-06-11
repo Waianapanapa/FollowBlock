df29d279fcfd81a56db333891a795e41c9c5d440
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-06-11 16:56:17 +0000

    Cleanups and version bump.

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schem
+ActiveRecord::Schema.define(version: 20150827021833) do
+
+  # These are extensions that must be en
