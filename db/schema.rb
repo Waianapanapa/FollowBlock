c86e8cc72b499ea0b75841e32cba04d6488298b9
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-07-31 12:58:56 +0000

    Ember VCL reset

--- db/schema.rb
+++ db/schema.rb
@@ -63,4 +63,5 @@ ActiveRecord::Schema.define(version: 20150827021833) do
     t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime 
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          defau
