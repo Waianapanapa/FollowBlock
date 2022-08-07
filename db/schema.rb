1b0dc608eff776784f1aacf900fb88fc3e09fc2d
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-08-07 18:55:18 +0000

    Added new test

--- db/schema.rb
+++ db/schema.rb
@@ -75,4 +75,5 @@ ActiveRecord::Schema.define(version: 20150827021833) do
     t.string   "role",                   default: "regular", null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["profile"], name: "index_users_on_profile
