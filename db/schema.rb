fc6c763f7bc3241e02465b2541f48070b7265e6f
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-08-07 21:47:11 +0000

    Better documentation.

--- db/schema.rb
+++ db/schema.rb
@@ -77,4 +77,6 @@ ActiveRecord::Schema.define(version: 20150827021833) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["profile"], name: "index_users_on_profile", using: :gin
-  add_index "users", ["reset_password_token"], name: "index_users_
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "bids",
