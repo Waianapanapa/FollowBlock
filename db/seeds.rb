e8eed9fe5e60933aa350fc253314323c22330c34
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-08-13 22:54:53 +0000

    update background color

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-# This file should contain all the record creation needed to see
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be load
