7b8fc1dee4a4738dd2c330f2c8acd8aa9a56458c
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-08-17 12:29:54 +0000

    Fixed missing imports

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' 
