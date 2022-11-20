44bf39a71bfde88c07d55301deb68a4ecd176ad7
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-11-20 12:27:49 +0000

    0.19

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "john#{n}@doe.com" }
     password { "password" }
-    password_confirmation
+    password_confirmation { "password" }
+    phone { "1122223333" }
+
+    trait :company do
+     
