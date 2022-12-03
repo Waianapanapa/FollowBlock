3cd10a5272589cc7193ba4f48ac3a6956a9718be
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-12-03 20:17:00 +0000

    Main Program

--- spec/features/sessions/sign_in_spec.rb
+++ spec/features/sessions/sign_in_spec.rb
@@ -2,4 +2,7 @@ require "rails_helper"
 
 RSpec.feature "User sign in" do
   before do
-    create(:user, :individual, email: "john.
+    create(:user, :individual, email: "john.doe@example.com", password: "password")
+  end
+
+  context "with val
