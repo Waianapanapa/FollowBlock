bb87bb571f65778252d373385ad6ff386eacada1
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2022-12-17 19:55:08 +0000

    Test mqtt.publish

--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -2,4 +2,7 @@ require "rails_helper"
 
 RSpec.describe Bid, type: :model do
   describe "relationships" do
-    it { is_expected.to belong_to(
+    it { is_expected.to belong_to(:user) }
+
+    it { is_expected.to have_many(:messages) }
+    it 
