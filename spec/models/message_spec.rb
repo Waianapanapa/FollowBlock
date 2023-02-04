c04bb3cd10b83015e71c37fec2e56255effafe0b
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-02-04 10:18:21 +0000

    Commit from LuCI Translation Portal by user jow.: 31 of 31 messages translated (0 fuzzy).

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,4 +2,8 @@ require "rails_helper"
 
 RSpec.describe Message, type: :model do
   describe "relationships" do
-    it { is_expected.to belong_
+    it { is_expected.to belong_to(:bid) }
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe "val
