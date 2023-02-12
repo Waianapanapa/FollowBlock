47f15937c93d527257b0626bfe49089a1b26d1b0
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-02-12 11:12:23 +0000

    Fix menu

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,8 @@ require "rails_helper"
 
 RSpec.describe Order, type: :model do
   describe "relationships" do
-    it { is_expected.to belong_to(:
+    it { is_expected.to belong_to(:bid) }
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe "validations"
