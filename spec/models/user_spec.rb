be0afdab37134fbabbfa70bf916c83548391b281
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-02-13 10:29:38 +0000

    some improvements

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 RSpec.describe User do
   describe "relationships" do
-    it { is_expected.to ha
+    it { is_expected.to have_many(:bids).dependent(:destroy) }
+    it { is_expected.to have_many(:orders).depen
