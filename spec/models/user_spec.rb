1bea4e164a7eb21a22ce234615030d1ce4dbd708
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-02-17 18:10:48 +0000

    Update pg.md

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,8 @@ require "rails_helper"
 RSpec.describe User do
   describe "relationships" do
     it { is_expected.to have_many(:bids).dependent(:destroy) }
-    it { is_expected.to have_many(:orders).depen
+    it { is_expected.to have_many(:orders).dependent(:destroy) }
+  end
+
+  describe "validations" do
+    it { is_expecte
