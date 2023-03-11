c1e13dd75dde0ad1b5d1e0c0eb887660b29fc11c
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-03-11 15:16:24 +0000

    Release 1.2.8

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,3 +3,6 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 
 require "spec_helper"
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| r
