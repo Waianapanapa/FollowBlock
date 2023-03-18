bc504c0b09ba6322b6febbd2831ee73fa7c71181
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-03-18 15:04:50 +0000

    Added new features

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    expectations.
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.sy
