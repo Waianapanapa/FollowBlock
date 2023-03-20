ee66a211932cac5c2dcb763a656a73e56d4e847f
Waianapanapa <110913573+Waianapanapa@users.noreply.github.com>
2023-03-20 16:52:20 +0000

    Fixed issue #13

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,4 +2,6 @@ require "capybara/rails"
 require "capybara/rspec"
 require "capybara/poltergeist"
 
-Capybara.configure do |c
+Capybara.configure do |config|
+  config.always_include_port = true
+  config.default_driv
